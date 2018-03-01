5bd96d67b5716a6300a30cce02f2574348fe9ad5
ACER <danducokrodimurti@gmail.com>
2018-03-01 17:56:04 +0700

    update passenger

--- src/sim/Passenger.java
+++ src/sim/Passenger.java
@@ -11,10 +11,12 @@ public class Passenger {
     int destination;
     double time;
     int timeload;
+    int timeunload;
     
     Passenger(double t){
         time = t;
         Random r = new Random();
         timeload = r.nextInt(11)+15;
+        timeunload = r.nextInt(9)+16;
     }
 }
