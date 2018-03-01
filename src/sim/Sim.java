7893f251307f3701ddd3826a3cf7b7f5e63dbb22
HafizhHD <hafizhdary@gmail.com>
2018-03-01 17:33:28 +0700

    update main

--- src/sim/Sim.java
+++ src/sim/Sim.java
@@ -5,6 +5,9 @@
  */
 package sim;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author ACER
@@ -19,16 +22,28 @@ public class Sim {
         s[0] = new Station(1, 3600/14);
         s[1] = new Station(2, 3600/10);
         s[2] = new Station(3, 3600/24);
-        
-        int i = 2;
+        s[0].Ar.start();
+		s[1].Ar.start();
+		s[2].Ar.start();
+		
         while(time<80*60*60){
-            for (int j = 0; j<s[i].Ar.passengers.size(); j++){
+			int i = b.position;
+			int k=0;
+			time += b.timetoload();
+			b.removepassenger();
+            for (int j = 0; j<s[i].passengers.size(); j++){
+				//System.out.println("Jml passenger: " + b.passengers.size());
+				s[i].updateQueue();
                 if(b.passengers.size()<20){
-                    b.addpassenger(s[i].Ar.passengers.get(j));
-                    time += s[i].Ar.passengers.get(j).timeload;
+                    b.addpassenger(s[i].passengers.get(j));
+					k=j;
+                    time += s[i].passengers.get(j).timeload;
                 }
             }
-            
+			for(int j=0;j<=k;j++) {
+				s[i].passengers.remove(j);
+			}
+            b.move();
         }
     }
 }
