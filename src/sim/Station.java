ca0815eb356900d40bb841f5844895658d062b5a
ACER <danducokrodimurti@gmail.com>
2018-03-01 17:44:09 +0700

    update station

--- src/sim/Station.java
+++ src/sim/Station.java
@@ -5,12 +5,58 @@
  */
 package sim;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Station {
     Arrivals Ar;
+    List<Passenger> passengers = new ArrayList<>();
     int id;
+    Random r = new Random();
     
     Station(int i, double mean){
         id = i;
         Ar = new Arrivals(mean);
+    }
+    
+    void updateQueue(){
+        int i = 0;
+        int s;
+        
+        if(id!=2){
+            Ar.passengers.get(0).destination=2;
+        } else{
+            s = r.nextInt(1000)+1;
+            if(s<=583){
+                Ar.passengers.get(0).destination=0;
+            } else  Ar.passengers.get(0).destination=1;
+        }
+        passengers.add(Ar.passengers.get(0));
+        
+        while(i-1<Ar.passengers.size()){
+            Passenger p = Ar.passengers.get(i);
+            Passenger q = Ar.passengers.get(i+1);
+            if(q.time-p.time>300){
+                break;
+            }else{
+                if(id!=2){
+                    q.destination=2;
+                } else{
+                    s = r.nextInt(1000)+1;
+                    if(s<=583){
+                        q.destination = 0;
+                    } else q.destination=1;
+                }
+                passengers.add(q);
+
+                i++;
+            }
+        }
+        
+        for(int j =0;j<=i;j++){
+            Ar.passengers.remove(j);
+        }
+        
     }
 }
